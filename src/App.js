ea30da84757cf164959c3cb0350a31c241d168fa
rockyFierro <finnslighthouse@gmail.com>
2021-06-13 15:35:40 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import NasaMedia from "./components/NasaMedia";
 
 function App() {
 
-  return (
-    <Router>
-        <main>
-            <Header/>
+    return (
+        <Router>
+            <main>
+                <Header/>
 
-            <Switch>
-                <Route exact path='/'>
-                    Hello there...
-                </Route>
-                <Route path='/nasa'>
-                    <NasaMedia/>
-                </Route>
-            </Switch>
-        </main>
-    </Router>
-  );
+                <Switch>
+                    <Route exact path='/'>
+                        <div>
+                            <p>
+                            Hello there... my name is <h2>Rocky Fierro</h2>.
+                                Follow me on
+                                    things like the
+                            </p>
+                            <a href="https://twitter.com/fierro_rocky">twitter</a>
+                                or maybe at
+                            <a href="https://www.risktofall.com"> my website</a>
+                        </div>
+                    </Route>
+
+                    <Route path='/nasa'>
+                        <NasaMedia/>
+                    </Route>
+                </Switch>
+            </main>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import './Header.css'
 
 const Header = () => {
-    return(
-    <Link to='/nasa'>
-        <button>Nasa</button>
-    </Link>
-)}
+    return (
+        <div className='header'>
+            <Link to='/nasa'>
+                <button>Nasa</button>
+            </Link>
+            <Link to='/'>
+                <button>
+                    Home
+                </button>
+            </Link>
+        </div>
+    )
+}
 export default Header;
+
